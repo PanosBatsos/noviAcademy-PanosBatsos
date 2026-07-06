fa8aa0fcfa889b1f3c2faa0f98063fffa2ec1493
Panos Batsos <panagiotismpatsos@gmail.com>
2026-07-06 23:29:36 +0300

    feat: add AddPlayer choice and menu

--- WorldRank/Program.cs
+++ WorldRank/Program.cs
@@ -1,1 +1,50 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using WorldRank;
+
+List<Player> players = new List<Player>();
+
+
+while (true)
+{
+    DisplayMenu();
+    string? choice = Console.ReadLine();
+    Console.WriteLine();
+
+    switch (choice)
+    {
+        case "1":
+            AddPlayer();
+            break;
+    }
+}
+
+
+void DisplayMenu()
+{
+    Console.WriteLine("\n=== Μενού Επιλογών ===");
+    Console.WriteLine("1. (Add player)");
+    Console.WriteLine("2. (List players)");
+    Console.WriteLine("3. (Find by name)");
+    Console.WriteLine("4. (Exit)");
+    Console.Write(" Select (1-4): ");
+} 
+
+void AddPlayer()
+{
+    Console.Write("Player's Name: ");
+    string? name = Console.ReadLine();
+    Player player = new Player(name ?? "");
+
+    Console.Write("Give score: ");
+    if (int.TryParse(Console.ReadLine(), out int score))
+    {
+        player.AddScore(score);
+        players.Add(player);
+        Console.WriteLine("Player added");
+    }
+    else
+    {
+        Console.WriteLine("Score must be an integer");
+    }
+   
+}
+
