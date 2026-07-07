bda11d99cb68019cfdf2de2e49589385e3fa589d
Panos Batsos <panagiotismpatsos@gmail.com>
2026-07-07 23:28:19 +0300

    feat: expand and refine menu to support new operations

--- WorldRank/Program.cs
+++ WorldRank/Program.cs
@@ -2,12 +2,15 @@
 
 List<Player> players = new List<Player>();
 
+IWalletRepository walletRepository = new InMemoryWalletRepository(players);
+IPlayerRepository playerRepository = new InMemoryPlayerRepository(players);
 
 while (true)
 {
     DisplayMenu();
     string? choice = Console.ReadLine();
     Console.WriteLine();
+    
 
     switch (choice)
     {
@@ -25,6 +28,18 @@ while (true)
         case "4":
             Console.WriteLine("Exiting...");
             return;
+        case "5":
+            LinkWallet();
+            break;
+        case "6":
+            ShowPlayersWallets();
+            break;
+        case "7":
+            RemovePlayerFromList();
+            break;
+        case "8":
+            GroupPlayersByScore();
+            break;
         default:
             Console.WriteLine("Not valid choice try again...");
             break;
@@ -39,7 +54,11 @@ void DisplayMenu()
     Console.WriteLine("2. (Display list)");
     Console.WriteLine("3. (Find by name)");
     Console.WriteLine("4. (Exit)");
-    Console.Write(" Select (1-4): ");
+    Console.WriteLine("5. (Link Player to Wallet)");
+    Console.WriteLine("6. (Display Player's wallets)");
+    Console.WriteLine("7. (Remove Player from List)");
+    Console.WriteLine("8. (Group Players by Score)");
+    Console.Write(" Select (1-8): ");
 } 
 
 void AddPlayer()
@@ -52,7 +71,7 @@ void AddPlayer()
     if (int.TryParse(Console.ReadLine(), out int score))
     {
         player.AddScore(score);
-        players.Add(player);
+        playerRepository.AddPlayer(player);
         Console.WriteLine("Player added");
     }
     else
@@ -95,3 +114,99 @@ void FindByName(string name)
     }
 }
 
+void LinkWallet()
+{
+    Console.Write("Enter Name: ");
+    string? name = Console.ReadLine();
+    Player? player = players.FirstOrDefault(p => p.Name.Equals(name ?? ""));
+
+    if (player == null)
+    {
+        Console.WriteLine("Player not found");
+        return;
+    }
+
+    Console.WriteLine("Select Currency (1: USD, 2: EUR): ");
+    string? currChoice = Console.ReadLine();
+    Currency selectedCurrency;
+    if (currChoice == "1")
+    {
+        selectedCurrency = Currency.USD;
+    }
+    else if (currChoice == "2")
+    {
+        selectedCurrency = Currency.EUR;
+    } else
+    {
+        Console.WriteLine("Not valid choice... Try again");
+        return;
+    }
+
+    Wallet wallet = new Wallet(selectedCurrency);
+    walletRepository.AddWallet(wallet, player.Id);
+
+    Console.WriteLine("Wallet succesfully linked to player: " + player.Name);
+}
+
+void ShowPlayersWallets()
+{
+    Console.Write("Enter Player's name to show wallets: ");
+    string? name = Console.ReadLine();
+    Player? player = players.FirstOrDefault(p => p.Name.Equals(name ?? ""));
+
+    if (player == null)
+    {
+        Console.WriteLine("Player not found");
+        return;
+    }
+
+    List<Wallet> userWallets = walletRepository.GetByPlayer(player.Id);
+    if (userWallets.Count == 0)
+    {
+        Console.WriteLine("This player has no wallets.");
+        return;
+    }
+
+    Console.WriteLine($"--- Wallets for {player.Name} ---");
+    foreach (Wallet wallet in userWallets)
+    {
+        Console.WriteLine($"- Currency: {wallet.Currency} | Balance: {wallet.Balance} | Blocked: {wallet.isBlocked}");
+    }
+}
+
+void RemovePlayerFromList()
+{
+    Console.Write("Enter Player's name: ");
+    string? name = Console.ReadLine();
+    Player? player = players.FirstOrDefault(p => p.Name.Equals(name ?? ""));
+
+    if (player == null)
+    {
+        Console.WriteLine("Player does not exist");
+        return;
+    }
+
+    playerRepository.DeletePlayer(player.Id);
+    Console.WriteLine("Player removed from List succesfully");
+}
+
+void GroupPlayersByScore()
+{
+    IEnumerable<IGrouping<int, Player>> playersGroup = playerRepository.GrouPlayersByScore();
+
+    if (!playersGroup.Any())
+    {
+        Console.WriteLine("No players to group");
+        return;
+    }
+
+    foreach (IGrouping<int, Player> group in playersGroup)
+    {
+        Console.WriteLine("Score: " + group.Key);
+
+        foreach (Player player in group)
+        {
+            Console.WriteLine(player.ToString());
+        }
+    }
+}
